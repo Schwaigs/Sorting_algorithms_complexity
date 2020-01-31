5f92be2073fb3ca3a19aadec50547d694e59d31b
Sarah Schwaiger <sarah.schwaiger@etu.unistra.fr>
2020-01-31 21:21:59 +0100

    Ajout algo 1 du tri par insertion

--- TP1/main.c
+++ TP1/main.c
@@ -3,18 +3,23 @@
 #include "utils.h"
 
 int main(int argc, char* argv[]) {
-    /********************* Test algo 2 ***********************/
-    long tab[] = {1,9,8,7,3,5,6};
+    long tab[5] = {3,8,1,5,2};
+    long tab2[8] = {12,8,1,-5,2,-8,6,9};
+    long tab3[6] = {7,8,1,2,-8,3};
+    long tab4[7] = {1,9,8,7,3,5,6};
 
-    triFusion(tab,7);
-    printf("tab : %ld, %ld, %ld, %ld, %ld, %ld, %ld \n",tab[0],tab[1],tab[2],tab[3],tab[4],tab[5],tab[6]);
+    /********************* Test algo 1 tri par insertion ***********************/
+    triInsertion(tab,5);
+    triInsertion(tab2,8);
+    triInsertion(tab3,6);
 
-  /********************* Test algo 3 ***********************/
-//  long tab[5] = {3,8,1,5,2};
-//  triRapide(tab,5);
-//  long tab2[8] = {12,8,1,-5,2,-8,6,9};
-//  triRapide(tab2,8);
-//  long tab3[6] = {7,8,1,2,-8,3};
-//  triRapide(tab3,6);
-  return 0;
+    /********************* Test algo 2 tri par fusion ***********************/
+    triFusion(tab4,7);
+    printf("tab : %ld, %ld, %ld, %ld, %ld, %ld, %ld \n",tab4[0],tab4[1],tab4[2],tab4[3],tab4[4],tab4[5],tab4[6]);
+
+    /********************* Test algo 3 tri rapide ***********************/
+    triRapide(tab,5);
+    triRapide(tab2,8);
+    triRapide(tab3,6);
+    return 0;
 }
