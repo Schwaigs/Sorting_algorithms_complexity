78fe265d8527aa72c476e55d8245e58e4eca4463
Yoann067 <yoann.louazel@gmail.com>
2020-02-07 13:46:41 +0100

    structure perf

--- TP1/utils.h
+++ TP1/utils.h
@@ -2,6 +2,14 @@
 #include <stdio.h>
 
 
+typedef struct p {
+    int nb_ecriture;
+    int nb_echange;
+    int nb_comp;
+    int appels_fusion
+    int appels_partition
+} perf;
+
 void cpTab(long* tab1, long* tab2, size_t deb, size_t fin);
 /********************************* Affichage ********************************/
 void printTab(long* A,size_t taille);
