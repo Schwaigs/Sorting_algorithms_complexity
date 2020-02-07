1d8d96a2a092f4102dd4e123eed321e6b1b286a2
Yoann067 <yoann.louazel@gmail.com>
2020-02-07 15:04:32 +0100

    printperf + trifusionD

--- TP1/main.c
+++ TP1/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include "tris.h"
+//#include "tris.h"
 #include "utils.h"
+#include "tri_fusion_decompte.h"
 
 int main(int argc, char* argv[]) {
     long tab[5] = {3,8,1,5,2};
     long tab2[8] = {12,8,1,-5,2,-8,6,9};
     long tab3[6] = {7,8,1,2,-8,3};
     long tab4[7] = {1,9,8,7,3,5,6};
-
+    perf p = init_perf();
     /********************* Test Gen Tab **********************/
     long tabAlea[10];
     long tabCrois[10];
@@ -15,27 +16,33 @@ int main(int argc, char* argv[]) {
     createAleaTab(tabAlea,10,-5,5);
     createCroisTab(tabCrois,10,-10,5);
     createDecroisTab(tabDeCrois,10,-5,20);
-    printf("tab alea \n");
-    printTab(tabAlea,10);
-    printf("tab Crois \n");
-    printTab(tabCrois,10);
-    printf("tab Decrois \n");
-    printTab(tabDeCrois,10);
+//    printf("tab alea \n");
+//    printTab(tabAlea,10);
+//    printf("tab Crois \n");
+//    printTab(tabCrois,10);
+//    printf("tab Decrois \n");
+//    printTab(tabDeCrois,10);
 
     /********************* Test algo 1 tri par insertion ***********************/
-    triInsertion(tab,5);
-    triInsertion(tab2,8);
-    triInsertion(tab3,6);
+//    triInsertion(tab,5);
+//    triInsertion(tab2,8);
+//    triInsertion(tab3,6);
 
     /********************* Test algo 2 tri par fusion ***********************/
-    triFusion(tab4,7);
-    triFusion(tab3,6);
-    triFusion(tab2,8);
-    triFusion(tab,5);
+//    triFusion(tab4,7);
+//    triFusion(tab3,6);
+//    triFusion(tab2,8);
+//    triFusion(tab,5);
 
     /********************* Test algo 3 tri rapide ***********************/
-    triRapide(tab,5);
-    triRapide(tab2,8);
-    triRapide(tab3,6);
+//    triRapide(tab,5);
+//    triRapide(tab2,8);
+//    triRapide(tab3,6);
+
+    /********************* Test algo 2 tri par fusionD **********************/
+    triFusion(tab4,7,&p);
+//    triFusion(tab3,6);
+//    triFusion(tab2,8);
+//    triFusion(tab,5);
     return 0;
 }
---
+++ TP1/tri_fusion_decompte.h
@@ -0,0 +1,7 @@
+#include "utils.h"
+
+
+/***************************** Algo 2 : Tri fusion *****************************/
+void triFusion(long* A, size_t taill,perf* p);
+void sousTriFusion(long* A, size_t debut, size_t fin,perf* p);
+void fusion(long* A, size_t debut, size_t milieu, size_t fin,perf* p);
