e54e4b291b88f2212c086b7ba28a7d40d2898dc5
Schwaigs <sarah.schwaiger.cha@gmail.com>
2021-05-08 17:03:17 +0200

    clean repo

--- TP1/tri_rapide_decompte.h
+++
@@ -1,5 +0,0 @@
-#include "utils.h"
-
-void sousTriRapide(long * A,size_t p, size_t r, perf* pe);
-size_t partition(long *, size_t p, size_t r, perf* pe);
-void triRapide(long *, size_t n);
--- TP2/tri_inser_decompte.h
+++
@@ -1,2 +0,0 @@
-#include "utils.h"
-void triInsertion(long * A, size_t n);
